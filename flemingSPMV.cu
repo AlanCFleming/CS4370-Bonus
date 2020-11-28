19d49395af4a9afd67dd760a37d20aaf5f2c6874
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-11-27 23:50:09 -0500

    Added CPU based multiplication

--- flemingSPMV.cu
+++ flemingSPMV.cu
@@ -7,3 +7,17 @@
 //CONSTANTS
 #define MATRIXSIZE 2048
 #define BLOCKSIZE 1024
+
+void spmvCPU(int num_row, const float* Value, const int* col_idx, const int* row_ptr, const float* x, float* y) {
+	
+	//preform multiplication using CSR format
+	//loop over rows
+	for(int i=0; i < num_row, i++) {
+		float sum = 0;
+		//loop over non-zero elements
+		for(int = row_ptr[i]; j < row_ptr[i + 1]; j++){
+			sum += value[j] * x[col_idx[j]];
+		}
+		y[i] = sum;
+	}
+}
