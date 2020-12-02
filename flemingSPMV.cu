01b7af173a9e776d622a57b3916b0e1cba797086
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-12-02 16:21:11 -0500

    fixed validation

--- flemingSPMV.cu
+++ flemingSPMV.cu
@@ -150,7 +150,7 @@ int main( int argc, char** argv) {
 
 	//verify results
 	bool valid = true;
-	for(int i = 0; i < 256; i++) {	
+	for(int i = 0; i < num_row; i++) {	
 		if(resultCPU[i] != resultGPU[i]) {
 			valid = false;
 			break;
